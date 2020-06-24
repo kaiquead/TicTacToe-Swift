48bdb12dbc4c510b73aba0414a8e1f11acbcd930
kaiquead <36281679+kaiquead@users.noreply.github.com>
2020-06-24 18:25:42 -0300

    Added labels and buttons on InitialScreen. Created PvpViewController and PvcViewController. Added NavigationController.

---
+++ TicTacToe/Controller/PvcViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PvcViewController.swift
+//  TicTacToe
+//
+//  Created by Kaique Alves on 24/06/20.
+//  Copyright © 2020 Kaique Alves. All rights reserved.
+//
+
+import UIKit
+
+class PvcViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
