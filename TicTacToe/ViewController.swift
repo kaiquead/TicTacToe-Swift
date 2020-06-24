2e75c2db207fe0dee1d2ea3351158d2c168fd265
kaiquead <36281679+kaiquead@users.noreply.github.com>
2020-06-24 17:12:12 -0300

    Initial commit.

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Kaique Alves on 24/06/20.
+//  Copyright © 2020 Kaique Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
