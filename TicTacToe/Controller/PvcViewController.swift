7f0d8699f8e4c750e42c0ab6880b75d920983e00
kaiquead <36281679+kaiquead@users.noreply.github.com>
2020-06-30 17:33:45 -0300

    Created a layout and connections of PvcViewController.

--- TicTacToe/Controller/PvcViewController.swift
+++ TicTacToe/Controller/PvcViewController.swift
@@ -10,11 +10,77 @@ import UIKit
 
 class PvcViewController: UIViewController {
     
+    //views
+    @IBOutlet weak var finishGameView: UIView!
+    @IBOutlet weak var gameView: UIView!
+    @IBOutlet weak var blackView: UIView!
+    @IBOutlet weak var choseView: UIView!
+    
+    //outlet buttons
+    @IBOutlet weak var but0: UIButton!
+    @IBOutlet weak var but1: UIButton!
+    @IBOutlet weak var but2: UIButton!
+    @IBOutlet weak var but3: UIButton!
+    @IBOutlet weak var but4: UIButton!
+    @IBOutlet weak var but5: UIButton!
+    @IBOutlet weak var but6: UIButton!
+    @IBOutlet weak var but7: UIButton!
+    @IBOutlet weak var but8: UIButton!
+    
+    //score
+    @IBOutlet weak var lbScore: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
+    @IBAction func btn0(_ sender: Any) {
+    }
+    
+    @IBAction func btn1(_ sender: Any) {
+    }
+    
+    @IBAction func btn2(_ sender: Any) {
+    }
+    
+    @IBAction func btn3(_ sender: Any) {
+    }
+    
+    @IBAction func btn4(_ sender: Any) {
+    }
+    
+    @IBAction func btn5(_ sender: Any) {
+    }
+    
+    @IBAction func btn6(_ sender: Any) {
+    }
+    
+    @IBAction func btn7(_ sender: Any) {
+    }
+    
+    @IBAction func btn8(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func btnGotIt(_ sender: Any) {
+    }
+    
+    @IBAction func btnChoseO(_ sender: Any) {
+    }
+    
+    @IBAction func btnChoseX(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnPlayAgain(_ sender: Any) {
+    }
+    
+    @IBAction func btnGoToMenu(_ sender: Any) {
+    }
+    
+    
     
 }
 
